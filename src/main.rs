6bc31693518d98113e75659d042343cb73b664a4
Dave Harlan <david.k.harlan@gmail.com>
2018-07-08 13:58:19 -0400

    Fiddle with rules and pattern over them

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,27 @@ struct ExampleParser;
 
 fn main() {
 
-    let input  = "one 123";
+    let input  = "one true false 123";
 
     let pairs = ExampleParser::parse(Rule::thing, input)
         .unwrap_or_else(|e| panic!("{}", e));
 
     for pair in pairs {
         let span = pair.clone().into_span();
-
-        // for token in pair.tokens() {
-        //     println!("Token:   {:?}", token);
-        // }
-
-        println!("Rule:   {:?}", pair.as_rule());
-        println!("Span:   {:?}", span);
-        println!("Text:   {}", span.as_str());
-
-        for inner_pair in pair.into_inner() {
-            let inner_span = inner_pair.clone().into_span();
-            match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:   {}", inner_span.as_str()),
-                Rule::digit => println!("Digit:    {}", inner_span.as_str()),
+        //println!("{:?} => {:?}", pair.as_rule(), span.as_str());
+        let token_str = span.as_str();
+        match pair.as_rule() {
+            Rule::identifier => println!("ident = {:?}", token_str),
+            Rule::integer => match token_str.parse::<i64>() {
+                Ok(i) => println!("integer = {}", i),
+                Err(_) => println!("larger than i64 not yet supported (input was {:?})", token_str)
+            },
+            Rule::boolean => match token_str.as_ref() {
+                "true" => println!("boolean = {}", true),
+                "false" => println!("boolean = {}", false),
                 _ => unreachable!()
-            }
+            },
+            _ => println!("UNKNOWN pattern = {}", token_str)
         }
     }
 }
