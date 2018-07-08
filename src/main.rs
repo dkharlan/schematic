20051585217ee8aad20c3a6e6df442bca4d8a961
Dave Harlan <david.k.harlan@gmail.com>
2018-07-08 16:48:03 -0400

    Don't use pest's implicit whitespace

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,15 @@ struct ExampleParser;
 
 fn main() {
 
-    //println!("check 1");
+    //let input = "1 a 4b b1 one true false 123 123234234234234234234234235436 -10 \"foo\"";     // errors?
+    let input = "1 a 4 b1 one true false 123 123234234234234234234234235436 -10 \"foo\"";      // works
+    //let input = "1 a 4 b1 one true false 123 123234234234234234234234235436 -10 \"foo\"";      // errors
 
-    let input  = "one true false 123 123234234234234234234234235436 -10 \"foo\"";
-    //let input = "one";
-
-    //println!("check 2");
-
-    let pairs = ExampleParser::parse(Rule::main, input)
+    let pairs = ExampleParser::parse(Rule::things, input)
         .unwrap_or_else(|e| panic!("{}", e));
-
-    //println!("check 3");
 
     for pair in pairs {
         let span = pair.clone().into_span();
-        //println!("checking span {:?} => {:?}", pair.as_rule(), span.as_str());
         let token_str = span.as_str();
         match pair.as_rule() {
             Rule::identifier => println!("ident = {:?}", token_str),
@@ -42,14 +36,4 @@ fn main() {
             _ => println!("UNKNOWN pattern = {}", token_str)
         }
     }
-
-    //println!("check 4");
-
-    //let string_literal_pairs = ExampleParser::parse(Rule::string, "\"foobar\"").unwrap_or_else(|e| panic!("{}", e));
-    //println!("{:?}", string_literal_pairs);
-
-    //let stuff = ExampleParser::parse(Rule::main, "\"foobar\"").unwrap_or_else(|e| panic!("{}", e));
-    //println!("{:?}", stuff);
-
-    //println!("check 5");
 }
